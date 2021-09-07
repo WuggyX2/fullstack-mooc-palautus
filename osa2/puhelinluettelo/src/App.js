1d866490e4ff442b7ba557b52e724e63dd20dbf8
Matti Virkkunen <matti.virkkunen@ceili.fi>
2021-09-07 22:06:38 +0300

    teht 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import PersonsList from "./components/PersonsList";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]
+  );
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterTerm, setFilterTerm] = useState("");
 
+  const effectHook = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+    .catch(error =>  {console.error(error)})
+  }
+  
+  useEffect(effectHook , [])
+
   const personsToShow = persons.filter((person) =>
     person.name.toLowerCase().includes(filterTerm.toLowerCase())
   );
+
   const handleOnClick = (event) => {
     event.preventDefault();
 
@@ -60,6 +68,7 @@ const App = () => {
       <PersonsList persons={personsToShow} />
     </div>
   );
-};
+}
+
 
 export default App;
